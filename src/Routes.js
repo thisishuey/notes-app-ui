09382c4d1c63b4b8d6b986643675630cd433ed9c
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2018-09-11 16:57:31 -0400

    Add login container

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Home from './containers/Home';
+import Signup from './containers/Signup';
+import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 
 export default () =>
 	<Switch>
 		<Route path="/" exact component={Home} />
+		<Route path="/signup" component={Signup} />
+		<Route path="/login" component={Login} />
 		<Route component={NotFound} />
 	</Switch>;
---
+++ src/containers/Login.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import './Login.css';
+
+export default class Login extends Component {
+	state = {
+		email: '',
+		password: ''
+	};	
+
+	validateForm () {
+		return this.state.email.length > 0 && this.state.password.length > 0;
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			[event.target.id]: event.target.value
+		});
+	}
+
+	handlSubmit = (event) => {
+		event.preventDefault();
+	}
+
+	render () {
+		return (
+			<div className="Login">
+				<form onSubmit={this.handlSubmit}>
+					<FormGroup bsSize="large" controlId="email">
+						<ControlLabel>Email</ControlLabel>
+						<FormControl
+							autofocus
+							onChange={this.handleChange}
+							type="email"
+							value={this.state.email}
+						/>
+					</FormGroup>
+					<FormGroup bsSize="large" controlId="password">
+						<ControlLabel>Password</ControlLabel>
+						<FormControl
+							onChange={this.handleChange}
+							type="password"
+							value={this.state.password}
+						/>
+					</FormGroup>
+					<Button block bsSize="large" disabled={!this.validateForm()} type="submit">
+						Login
+					</Button>
+				</form>
+			</div>
+		);
+	}
+}
