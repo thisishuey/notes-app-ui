bc0c77d9c06be4cefa6e44adf8d32910dd7c59a1
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2018-09-11 15:01:05 -0400

    Customize react app with favicon, bootstrap, etc.

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+	s3: {
+		REGION: 'us-east-1',
+		BUCKET: 'carpe-telam-serverless-notes-app-uploads'
+	},
+	apiGateway: {
+		REGION: 'us-east-1',
+		URL: 'https://zy72eu0jt8.execute-api.us-east-1.amazonaws.com/dev'
+	},
+	cognito: {
+		REGION: 'us-east-1',
+		USER_POOL_ID: 'us-east-1_BSsHkCjvo',
+		APP_CLIENT_ID: '1r5pfgl6ini5us2sjqlr6imuem',
+		IDENTITY_POOL_ID: 'us-east-1:e50b48b0-fbe6-42bb-9243-5baa69510ddb'
+	}
+}
